42a8dc65862f4a4b743b875df342c43840f99e9e
Cunnjoel <cunningham.joel84@gmail.com>
2021-02-17 19:16:42 -0600

    update js.script

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables
+var lowercase = ["a", "b", "c", "d"];
+var uppercase = ["A", "B", "C","D"];
+var numbers = ["1", "2", "3", "4"];
+var specialChar = ["!", "@", "#", "$"];
+
+var lowcas = function(){
+  Math.floor(Math.random * lowercase.length);
+}
+var uppcase = function(){
+  Math.floor(Math.random * uppercase.length);
+}
+var numbs = function(){
+  Math.floor(Math.random * numbers.length);
+}
+var spec = function(){
+  Math.floor(Math.random * specialChar.length);
+}
+
+// starting states
+lowercase = "false"
+uppercase = "false"
+numbers = "false"
+specialChar = "false"
+
+// if statments
+var pass = function(){
+  if (lowercase === true){
+    lowcas()
+  }else if (uppercase === true){
+    uppcase()
+  }else if (numbers === true){
+    numbs()
+  }else (specialChar === true);{
+    spec()
+  }
+}
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
