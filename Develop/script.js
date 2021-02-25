27fc4de323acecc732cb6a59f25d9b1852b8c932
Cunnjoel <cunningham.joel84@gmail.com>
2021-02-24 18:13:24 -0600

    update repo

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,87 @@
+// array variables to be used in password
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+​
+// Function to prompt user for password options
+function getPasswordOptions() {
+  var passwordLength = Number(prompt("How many characters will your password be? Enter number between 8 and 128"));
+  var inclLow = confirm("Do you want to include lowercase characters?");
+  var inclUpp = confirm("Do you want to include uppercase characters?");
+  var inclNum = confirm("Do you want to include numbers?");
+  var inclSpe = confirm("Do you want to include special characters?");
+}
+
+  // Variable to store length of password from user input
+  var passLength = console.log(passwordLength);
+​
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (isNaN(passLength) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+​
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (passLength <8){
+    alert("Password length must be at least 8 characters long.");
+    passwordLength();
+  }
+
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+​   else if (passLength >128){
+  alert("Password length cannot be longer than 128 characters long.")
+}
+
+  // Variable to store boolean regarding the inclusion of special characters
+  var specialCharactersConfirm = console.log(inclSpe);
+​
+  // Variable to store boolean regarding the inclusion of numeric characters
+  var numericCharactersConfirm = console.log(inclNum);
+​
+  // Variable to store boolean regarding the inclusion of lowercase characters
+  var lowerCasedCharactersConfirm = console.log(inclLow);
+​
+  // Variable to store boolean regarding the inclusion of uppercase characters
+  var upperCasedCharactersConfirm = console.log(inclUpp);
+​
+  // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+  if (!specialCharactersConfirm && !numericCharactersConfirm && !lowerCasedCharactersConfirm && !upperCasedCharactersConfirm) {
+    alert('Must select at least one character type');
+    return;
+  }
+​
+  // Object to store user input variables
+  var passwordOptions = [];
+  console.log(passwordOptions)
+    
+  // Return the options object as the exported value of this function
+  return passwordOptions;
+​
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Variables
-var lowercase = ["a", "b", "c", "d"];
-var uppercase = ["A", "B", "C","D"];
-var numbers = ["1", "2", "3", "4"];
-var specialChar = ["!", "@", "#", "$"];
+
 
 var lowcas = function(){
   Math.floor(Math.random * lowercase.length);
